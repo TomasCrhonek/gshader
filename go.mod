c7781e5a5aade1027e36f9935fd6fb0d7623670e
Tomas Crhonek <tomas@heronovo.cz>
2021-08-16 22:04:59 +0200

    Better cli parse with pflags.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module heronovo.cz/gshader
 
 go 1.16
+
+require github.com/spf13/pflag v1.0.5
--- main.go
+++ main.go
@@ -4,21 +4,49 @@ import (
 	"image/png"
 	"log"
 	"os"
+	"runtime/trace"
+
+	flag "github.com/spf13/pflag"
 )
 
 const (
-	imgFile = "image.png"
-	width   = 10240
-	height  = 10240
+	DEFAULT_IMAGEFILENAME = "image.png"
+	DEFAULT_WIDTH         = 1024
+	DEFAULT_HEIGHT        = 1024
 )
 
 func main() {
-	f, err := os.Create(imgFile)
+	traceQ := flag.BoolP("trace", "t", false, "Trace the program for performance analysis.")
+	helpQ := flag.BoolP("help", "h", false, "This help page.")
+	imageFile := flag.StringP("name", "n", DEFAULT_IMAGEFILENAME, "Image `file` name.")
+	width := flag.IntP("width", "x", DEFAULT_WIDTH, "Image `width`.")
+	height := flag.IntP("height", "y", DEFAULT_HEIGHT, "Image `height`.")
+	flag.CommandLine.SortFlags = false
+	flag.Parse()
+
+	if *helpQ {
+		flag.Usage()
+		return
+	}
+
+	if *traceQ {
+		traceFile, err := os.Create("main.trace")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer traceFile.Close()
+		trace.Start(traceFile)
+		defer trace.Stop()
+	}
+
+	f, err := os.Create(*imageFile)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
 
-	if err := png.Encode(f, computeImage(width, height, shaderMandelbrot)); err != nil {
+	encoder := png.Encoder{CompressionLevel: png.NoCompression}
+	if err := encoder.Encode(f, computeImage(*width, *height, shaderMandelbrot)); err != nil {
 		log.Fatal(err)
 	}
 }
