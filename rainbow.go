fe1acd47de31da275489c9c3dbd61d52f685ac0a
Tomas Crhonek <tomas@heronovo.cz>
2021-08-17 11:37:45 +0200

    New colormap.

---
+++ rainbow.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"image/color"
+	"math"
+)
+
+func grayToRad(x uint8) float64 {
+	return (float64(x) / 255.0) * 2 * math.Pi
+}
+
+func offset(x float64) uint8 {
+	return uint8(127*x + 127)
+}
+
+func rainbow(c color.Gray) color.Color {
+	return color.RGBA{
+		R: offset(math.Sin(grayToRad(c.Y))),
+		G: offset(math.Sin(grayToRad(c.Y) + math.Pi/2)),
+		B: offset(math.Sin(grayToRad(c.Y) + math.Pi)),
+		A: 255,
+	}
+}
